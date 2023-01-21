eb063d539f6286596c39dd3efa27dcc44f6ff91a
Alexey Ramazanov <lion-rav@mail.ru>
2023-01-21 11:15:33 +0300

    Код создан

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string[] array1 = new string[5] {"Russia", "Kazan", "D", "Yes", "2"};
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
+{
+    int num = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[num] = array1[i];
+            num++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+    Console.WriteLine();
+
+NewArray(array1, array2);
+
+PrintArray(array2);
